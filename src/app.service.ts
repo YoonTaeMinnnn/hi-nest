8527cdf653c28c4da6f4bb6120776758c5f44337
YoonTaeminnnn <xoals4340@hs.ac.kr>
2023-10-21 00:36:20 +0900

    architechure

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!!!';
+  }
+
+  getHi(): string {
+    return "hi nest";
   }
 }
